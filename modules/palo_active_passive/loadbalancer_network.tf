9178420d204c12c82376ed15b7ccd52b02845544
George Benetatos <bene@otenet.gr>
2024-07-02 15:30:15 +0300

    zeros

--- modules/palo_active_passive/loadbalancer_network.tf
+++ modules/palo_active_passive/loadbalancer_network.tf
@@ -323,7 +323,7 @@ resource "azurerm_lb_rule" "int_p_allports" {
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "int_p_egress_backend0" {
-  network_interface_id    = azurerm_network_interface.int_p_trust0.id
+  network_interface_id    = azurerm_network_interface.int_p_trust00.id
   ip_configuration_name   = "ipconfig1"
   backend_address_pool_id = azurerm_lb_backend_address_pool.int_p_lbegress_backend_address_pool.id
 }
@@ -378,7 +378,7 @@ resource "azurerm_lb_rule" "int_q_allports" {
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "int_q_egress_backend0" {
-  network_interface_id    = azurerm_network_interface.int_q_trust0.id
+  network_interface_id    = azurerm_network_interface.int_q_trust00.id
   ip_configuration_name   = "ipconfig1"
   backend_address_pool_id = azurerm_lb_backend_address_pool.int_q_lbegress_backend_address_pool.id
 }
